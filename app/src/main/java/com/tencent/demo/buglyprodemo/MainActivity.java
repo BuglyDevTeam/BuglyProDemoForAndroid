6435fe634db3fffdb3b2055eb002a609bd6d1a48
lilyxie <lilyxie@tencent.com>
2023-04-20 22:35:51 +0800

    fix: 更新SDK版本并解决编译问题
    - 更新SDK版本并解决编译问题

--- app/src/main/java/com/tencent/demo/buglyprodemo/MainActivity.java
+++ app/src/main/java/com/tencent/demo/buglyprodemo/MainActivity.java
@@ -1,11 +1,13 @@
 package com.tencent.demo.buglyprodemo;
 
 import android.app.Application;
+import android.os.Build;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import com.tencent.bugly.library.Bugly;
 import com.tencent.bugly.library.BuglyBuilder;
-import com.tencent.bugly.library.BuglyConstants;
+import com.tencent.bugly.library.BuglyLogLevel;
+import com.tencent.bugly.library.BuglyMonitorName;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,11 +22,17 @@ public class MainActivity extends AppCompatActivity {
         Application application = getApplication();
         String appKey = "b860b0bc-fe8d-4356-8ba5-2ff74e21c48c";
         String appID = "ae9c799533";
-        String userID = "test_user_id";
-        String uniqueID = "test_unique_id";
+
         BuglyBuilder buglyBuilder = new BuglyBuilder(appID, appKey);
-        buglyBuilder.setProperty(BuglyConstants.BUILD_USER_ID, userID);
-        buglyBuilder.setProperty(BuglyConstants.BUILD_UNIQUE_ID, uniqueID);
+        buglyBuilder.appVersion = "4.5.8";
+        buglyBuilder.buildNumber = "12";
+        buglyBuilder.userId = "test_user_id";
+        buglyBuilder.uniqueId = "test_unique_id";
+        buglyBuilder.appVersionType = "Gray";
+        buglyBuilder.monitorList = BuglyMonitorName.ALL_MONITOR;
+        buglyBuilder.deviceModel = Build.MODEL;
+        buglyBuilder.enableCrashProtect = true;
+        buglyBuilder.logLevel = BuglyLogLevel.LEVEL_DEBUG;
         Bugly.init(application, buglyBuilder);
     }
 }
